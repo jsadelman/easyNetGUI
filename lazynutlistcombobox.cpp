d97cb1ae0144a94fd136afb121bd19af066ad9b4
unknown <pscjd@bristol.ac.uk>
2015-05-18 21:26:54 +0100

    Switched to new lazyNutListComoboBox

--- lazynutlistcombobox.cpp
+++ lazynutlistcombobox.cpp
@@ -1,13 +1,18 @@
 #include "lazynutlistcombobox.h"
 #include "lazynutjobparam.h"
 #include "sessionmanager.h"
+#include <QDebug>
+#include <QMouseEvent>
+#include <QShowEvent>
+#include <QCoreApplication>
+#include <QPoint>
+
 
 LazyNutListComboBox::LazyNutListComboBox(QString getListCmd, QWidget *parent) :
-    getListCmd(getListCmd), QComboBox(parent)
+    getListCmd(getListCmd), eventSwitch(true), QComboBox(parent)
 {
     setEditable(false);
-    addItem("-empty-");
-    setSizeAdjustPolicy(QComboBox::AdjustToContents);
+    eventParams.clear();
 }
 
 void LazyNutListComboBox::setEmptyItem(bool present)
@@ -26,30 +31,179 @@ void LazyNutListComboBox::setGetListCmd(QString cmd)
 
 void LazyNutListComboBox::getList(QString cmd)
 {
-//    buildList(QStringList({"badger", "mushroom", "snake"}));
-
-    qDebug() << cmd;
-    getListCmd = cmd.isEmpty() ? getListCmd : cmd;
-    if (!getListCmd.isEmpty())
+    if (!SessionManager::instance()->isReady())
     {
-        LazyNutJobParam *param = new LazyNutJobParam;
-        param->logMode |= ECHO_INTERPRETER; // debug purpose
-        param->cmdList = QStringList({QString("xml %1").arg(getListCmd)});
-        param->answerFormatterType = AnswerFormatterType::ListOfValues;
-        param->setAnswerReceiver(this, SLOT(buildList(QStringList)));
-        SessionManager::instance()->setupJob(param, sender());
+        emit listReady();
+        repostEvent();
     }
     else
     {
-        clear();
-        emit listReady();
+        savedCurrentText = currentText();
+        getListCmd = cmd.isEmpty() ? getListCmd : cmd;
+        if (!getListCmd.isEmpty())
+        {
+            LazyNutJobParam *param = new LazyNutJobParam;
+            param->logMode |= ECHO_INTERPRETER; // debug purpose
+            param->cmdList = QStringList({QString("xml %1").arg(getListCmd)});
+            param->answerFormatterType = AnswerFormatterType::ListOfValues;
+            param->setAnswerReceiver(this, SLOT(buildList(QStringList)));
+            SessionManager::instance()->setupJob(param, sender());
+        }
+        else
+        {
+            emit listReady();
+            repostEvent();
+        }
     }
+}
+
+
+bool LazyNutListComboBox::event(QEvent *event)
+{
+    switch (event->type())
+    {
+    case QEvent::MouseButtonPress:
+    {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+        // if left click
+        if (mouseEvent->button() == Qt::LeftButton &&
+            mouseEvent->buttons() == Qt::LeftButton &&
+            mouseEvent->modifiers() == Qt::NoModifier
+            )
+        {
+            // if not waiting for data from lazyNut
+            if (eventMutex.tryLock())
+            {
+                // if event generated outside, i.e. not reposted from this class,
+                // i.e. if lazyNut has not been called yet on this event
+                if (eventSwitch)
+                {
+                    eventParams.type = mouseEvent->type();
+                    eventParams.localPos = mouseEvent->pos();
+                    eventParams.button = mouseEvent->button();
+                    eventParams.buttons = mouseEvent->buttons();
+                    eventParams.modifiers = mouseEvent->modifiers();
+
+                    getList();
+                    // eventMutex unlocked in repostEvent()
+                    // eventSwitch assigned to false in repostEvent()
+                }
+                else // the event has been reposted from this class
+                {
+                    // pass event to base class event handler
+                    eventSwitch = true;
+                    eventMutex.unlock();
+                    return QComboBox::event(event);
+                }
+            }
+            // else do nothing, return true at the end
+        }
+        else // pass event to base class event handler
+            return QComboBox::event(event);
+
+        break;
+    }
+    case QEvent::MouseButtonDblClick:
+    {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+        // ignore left double clicks: a double click has the same effect of a single click
+        if (mouseEvent->button() == Qt::LeftButton &&
+            mouseEvent->buttons() == Qt::LeftButton &&
+            mouseEvent->modifiers() == Qt::NoModifier
+            )
+            break; // return true at the end
+
+        else
+            return QComboBox::event(event);
+    }
+
+    case QEvent::Show:
+    {
+        if (eventMutex.tryLock())
+        {
+            if (eventSwitch)
+            {
+                eventParams.type = event->type();
+                getList();
+            }
+            else
+            {
+                eventSwitch = true;
+                eventMutex.unlock();
+                return QComboBox::event(event);
+            }
+        }
+        else // pass event to base class event handler
+            return QComboBox::event(event);
+
+        break;
+    }
+    default:
+        return QComboBox::event(event);
+    }
+    return true;
 }
 
 void LazyNutListComboBox::buildList(QStringList list)
 {
     clear();
     addItems(list);
+    setCurrentText(savedCurrentText);
     emit listReady();
+    repostEvent();
 }
 
+
+void LazyNutListComboBox::repostEvent()
+{
+    if (eventSwitch) // safety, should always be true
+    {
+        eventSwitch = false;
+        eventMutex.unlock();
+        switch (eventParams.type)
+        {
+        case QEvent::MouseButtonPress:
+        {
+            QCoreApplication::postEvent(this, new QMouseEvent(eventParams.type, eventParams.localPos,
+                                                              eventParams.button, eventParams.buttons, eventParams.modifiers));
+            break;
+        }
+        case QEvent::Show:
+        {
+            QCoreApplication::postEvent(this, new QShowEvent());
+            break;
+        }
+        default:
+            ;
+        }
+        eventParams.clear();
+    }
+}
+
+
+
+void LazyNutListComboBox::__debug_getList(QString /*cmd*/)
+{
+    // a debug version of getList()
+    // at every call it creates a new layer starting from A and proceeding alphabetically
+    // and asks lazyNut the list of layers. In this way the list is always different at every call
+    // and the size increases.
+    if (!SessionManager::instance()->isReady())
+    {
+        emit listReady();
+        repostEvent();
+    }
+    else
+    {
+        savedCurrentText = currentText();
+        QStringList list;
+        list.append(QString("create layer %1").arg(QChar('A' + __debug_getList_counter++)));
+        list.append("xml list layer");
+        LazyNutJobParam *param = new LazyNutJobParam;
+        param->logMode |= ECHO_INTERPRETER; // debug purpose
+        param->cmdList = list;
+        param->answerFormatterType = AnswerFormatterType::ListOfValues;
+        param->setAnswerReceiver(this, SLOT(buildList(QStringList)));
+        SessionManager::instance()->setupJob(param, sender());
+    }
+}
--- lazynutlistcombobox.h
+++ lazynutlistcombobox.h
@@ -2,7 +2,28 @@
 #define LAZYNUTLISTCOMBOBOX_H
 
 #include <QComboBox>
-#include <QDebug>
+#include <QPoint>
+#include <Qt>
+#include <QEvent>
+#include <QMutex>
+
+struct EventParams
+{
+    QEvent::Type            type;
+    QPoint                  localPos;
+    Qt::MouseButton         button;
+    Qt::MouseButtons        buttons;
+    Qt::KeyboardModifiers   modifiers;
+
+    void clear()
+    {
+        type = QEvent::None;
+        localPos = QPoint();
+        button = Qt::NoButton;
+        buttons = Qt::NoButton;
+        modifiers = Qt::NoModifier;
+    }
+};
 
 class LazyNutListComboBox : public QComboBox
 {
@@ -18,11 +39,22 @@ public slots:
     void setGetListCmd(QString cmd);
     void getList(QString cmd = QString());
 
+protected:
+    bool event(QEvent *event) Q_DECL_OVERRIDE;
+
 private slots:
     void buildList(QStringList list);
+    void __debug_getList(QString = QString());
 
 private:
+    void repostEvent();
+
     QString getListCmd;
+    bool eventSwitch;
+    QMutex eventMutex;
+    EventParams eventParams;
+    QString savedCurrentText;
+    int __debug_getList_counter = 0;
 
 };
 
