f032994a16a91680f59bcfa3e619c011c57e32d2
unknown <pscjd@IT032554.users.bris.ac.uk>
2015-06-29 17:54:52 +0100

    doesn't run -- crash related to lexical_decider & descriptionupdater?

---
+++ trialwidget.cpp
@@ -0,0 +1,7 @@
+#include "trialwidget.h"
+
+TrialWidget::TrialWidget()
+{
+
+}
+
---
+++ trialwidget.h
@@ -0,0 +1,11 @@
+#ifndef TRIALWIDGET_H
+#define TRIALWIDGET_H
+
+
+class TrialWidget
+{
+public:
+    TrialWidget();
+};
+
+#endif // TRIALWIDGET_H
