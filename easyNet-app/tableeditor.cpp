jsadelman/easyNetGUI

commit a739bf99d7058d5fd2920cc4804a7b620b7603e8

Some dataframe fixes

easyNet-app/tableeditor.cpp
@@ -22,6 +22,10 @@
 TableEditor::TableEditor(const QString &tableName, QWidget *parent)
     : QMainWindow(parent)
 {
+    tableBox=nullptr;
+    thisIsParamWindow=false;
+    if (tableName=="Parameters")
+        thisIsParamWindow=true;
     // this style of constructor is used for stimSetForm
     init(tableName, parent);
 //    listTitle->hide();
@@ -31,10 +35,8 @@ TableEditor::TableEditor(const QString &tableName, QWidget *parent)
     // can't edit stimulus sets (at present)
     view->setEditTriggers(QAbstractItemView::NoEditTriggers);
     // but can edit parameter sets
-    if (tableName=="Parameters")
-    {
+    if (thisIsParamWindow)
         view->setEditTriggers(QAbstractItemView::AllEditTriggers);
-    }
     else if (tableName=="Debug_log")
         setViewToStringList(); // temp!!!
     else
@@ -49,6 +51,7 @@ TableEditor::TableEditor(const QString &tableName, QWidget *parent)
         view->setHorizontalHeader(myHeader);
         connect(myHeader, SIGNAL(columnDropped(QString)), this, SIGNAL(columnDropped(QString)));
         connect(myHeader, SIGNAL(restoreComboBoxText()), this, SIGNAL(restoreComboBoxText()));
+        connect(this,SIGNAL(newTableName(QString)),myHeader,SLOT(setTableName(QString)));
         connect(this,SIGNAL(newTableName(QString)),myHeader,SLOT(setTableName(QString)));
 
     }
@@ -75,17 +78,16 @@ void TableEditor::init(const QString &tableName, QWidget *parent)
     widget = new QWidget(this);
     setCentralWidget(widget);
     QVBoxLayout* layout = new QVBoxLayout(widget);
-    tableBox = new QComboBox(widget);
 
-/*
- *     editDisplayWidget = new QComboBox;
-    static_cast<QComboBox*>(editDisplayWidget)->setModel(levelsListModel);
-    setWidgetValue(raw2widgetValue(settingsElement["value"]()));
-    currentValue = settingsElement["value"]();
-    valueSet = !settingsElement["value"]().isEmpty();
-*/
-    layout->addWidget(tableBox);
-    connect(tableBox, SIGNAL(currentIndexChanged(QString)), this, SLOT(updateTableView(QString)));
+    qDebug() << "In TableEditor::init() tableName is " << tableName << "is it param window:" << thisIsParamWindow;
+    if (!thisIsParamWindow)
+    {
+        tableBox = new QComboBox(widget);
+        layout->addWidget(tableBox);
+//        connect(tableBox, SIGNAL(currentIndexChanged(QString)), this, SLOT(updateTableView(QString)));
+        // don't use currentIndexChanged(), because it'll lead to lots of gets during model load
+        connect(tableBox, SIGNAL(activated(QString)), this, SLOT(updateTableView(QString)));
+    }
 
     view = new QTableView(this);
 //    objectListView = new QListView(this);
@@ -157,18 +159,29 @@ void TableEditor::init(const QString &tableName, QWidget *parent)
 
 void TableEditor::setTableText(QString text)
 {
-    tableBox->addItem(text);
-    tableBox->setCurrentIndex(tableBox->findData(text,Qt::DisplayRole));
+    if (tableBox)
+    {
+        tableBox->addItem(text);
+        tableBox->setCurrentIndex(tableBox->findData(text,Qt::DisplayRole));
+    }
     emit newTableName(text);
+
 }
+
+//void TableEditor::setParamSetName(QString text)
+//{
+
+
+//}
 
 void TableEditor::selectTable(QString text)
 {
     qDebug() << "Received request to select " << text;
-
-    tableBox->setCurrentIndex(tableBox->findData(text,Qt::DisplayRole));
-    qDebug() << "Selected table is" << tableBox->currentText();
-
+    if (tableBox)
+    {
+        tableBox->setCurrentIndex(tableBox->findData(text,Qt::DisplayRole));
+        qDebug() << "Selected table is" << tableBox->currentText();
+    }
 }
 
 void TableEditor::setFilter(QString type)
@@ -267,7 +280,13 @@ void TableEditor::createActions()
 
 void TableEditor::refresh()
 {
-    updateTableView(tableBox->currentText());
+    qDebug() << "Entered refresh(), currentTable is " << currentTable;
+    if (tableBox)
+        qDebug() << " and tableBox->currentText is " << tableBox->currentText();
+    if (thisIsParamWindow)
+        updateTableView(currentTable);
+    else
+        updateTableView(tableBox->currentText());
 
 }
 
@@ -335,8 +354,9 @@ void TableEditor::setView(QString name)
 void TableEditor::addDataFrameToWidget(QDomDocument* domDoc)
 {
     dfModel = new DataFrameModel(domDoc, this); // you only need this line to load in the entire XML table
-    connect(dfModel, SIGNAL(newParamValueSig(QString)),
-            this,SIGNAL(newParamValueSig(QString)));
+    dfModel->setTableName(currentTable);
+    connect(dfModel, SIGNAL(newParamValueSig(QString,QString)),
+            this,SIGNAL(newParamValueSig(QString,QString)));
 
     view->setModel(dfModel);
     view->resizeColumnsToContents();
@@ -544,8 +564,9 @@ bool TableEditor::dropMimeData(const QMimeData * data, Qt::DropAction action, in
 
 void TableEditor::updateTableView(QString text)
 {
-//    qDebug() << this << "Entered updateTableView with " << text;
-//    qDebug() << "currentIndex = " << view->currentIndex();
+    qDebug() << this << "Entered updateTableView with " << text;
+    qDebug() << "currentIndex = " << view->currentIndex();
+    qDebug() << "is this param window?" << thisIsParamWindow;
     if (!text.size())
         return;
     if (text=="Untitled")

easyNet-app/dataframemodel.cpp
@@ -73,13 +73,17 @@ bool DataFrameModel::setData(const QModelIndex & index, const QVariant & value, 
         tBody().childNodes().at(index.row() + 1).childNodes().at(index.column() +1).firstChild().setNodeValue(value.toString());
         QString newParamValue = tBody().childNodes().at(index.row() + 1).childNodes().at(index.column()).toElement().text() +
                 " " + value.toString();
-        emit newParamValueSig(newParamValue);
+        emit newParamValueSig(tableName,newParamValue);
 
     }
     return true;
 }
 
-
+void DataFrameModel::setTableName(QString name)
+{
+    tableName = name;
+    qDebug() << "DataFrameModel: tableName set to " << tableName;
+}
 
 
 Qt::ItemFlags DataFrameModel::flags (const QModelIndex &index) const
@@ -202,4 +206,5 @@ void DataFrameHeader::performDrag()
 void DataFrameHeader::setTableName(QString name)
 {
     tableName = name;
+    qDebug() << "DataFrameHeader: tableName set to " << tableName;
 }

easyNet-app/dataframemodel.h
@@ -54,8 +54,10 @@ public:
 
     bool setData(const QModelIndex &index, const QVariant &value, int role);
 
-    signals:
-    void newParamValueSig (QString);
+public slots:
+    void setTableName(QString name);
+signals:
+    void newParamValueSig (QString,QString);
 
 private slots:
 private:

easyNet-app/descriptionupdater.cpp
@@ -29,7 +29,7 @@ void DescriptionUpdater::setProxyModel(QSortFilterProxyModel *proxy)
 
 void DescriptionUpdater::wakeUpUpdate()
 {
-    qDebug() << this << "wakeUpUpdate";
+//    qDebug() << this << "wakeUpUpdate";
     connect(proxyModel, SIGNAL(dataChanged(QModelIndex,QModelIndex)),
             this, SLOT(requestDescriptions(QModelIndex,QModelIndex)));
     connect(proxyModel, SIGNAL(rowsInserted(QModelIndex,int,int)),
@@ -38,7 +38,7 @@ void DescriptionUpdater::wakeUpUpdate()
 
 void DescriptionUpdater::goToSleep()
 {
-    qDebug() << this << "go to sleep ";
+//    qDebug() << this << "go to sleep ";
     disconnect(proxyModel, SIGNAL(dataChanged(QModelIndex,QModelIndex)),
             this, SLOT(requestDescriptions(QModelIndex,QModelIndex)));
     disconnect(proxyModel, SIGNAL(rowsInserted(QModelIndex,int,int)),

easyNet-app/easyNet-app.pro
@@ -22,7 +22,7 @@ DEPENDPATH =  .. .
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-TARGET = easyNet9
+TARGET = easyNet12
 TEMPLATE = app
 
 

easyNet-app/easyNetMainWindow.cpp
@@ -203,10 +203,10 @@ void EasyNetMainWindow::connectSignalsAndSlots()
     connect(modelComboBox, SIGNAL(currentIndexChanged(QString)),
             SessionManager::instance(), SLOT(setCurrentModel(QString)));
     connect(this,SIGNAL(newTableSelection(QString)),tablesWindow,SLOT(updateTableView(QString)));
-    connect(paramEdit,SIGNAL(setParamDataFrameSignal(QString)),
-             this,SLOT(setParamDataFrame(QString)));
-    connect(paramEdit, SIGNAL(newParamValueSig(QString)),
-            this,SLOT(setParam(QString)));
+//    connect(paramEdit,SIGNAL(setParamDataFrameSignal(QString)),
+//             this,SLOT(setParamDataFrame(QString)));
+    connect(paramEdit, SIGNAL(newParamValueSig(QString,QString)),
+            this,SLOT(setParam(QString,QString)));
     connect(plotSettingsWindow, SIGNAL(plot(QString,QByteArray)),
             plotViewer,SLOT(loadByteArray(QString,QByteArray)));
     connect(plotSettingsWindow, SIGNAL(newPlotSignal(QString)),
@@ -306,10 +306,11 @@ void EasyNetMainWindow::setParamDataFrame(QString name)
     paramEdit->setTableText(name);
 }
 
-void EasyNetMainWindow::setParam(QString newParamValue)
+void EasyNetMainWindow::setParam(QString paramDataFrame, QString newParamValue)
 {
     QString cmd1 = paramDataFrame + " set " + newParamValue;
     QString cmd2 = QString("xml ") + paramDataFrame + " get ";
+    qDebug() << "Called setParam():" << newParamValue;
     SessionManager::instance()->runCmd({cmd1,cmd2});
 }
 

easyNet-app/easyNetMainWindow.h
@@ -122,7 +122,7 @@ private slots:
     void showDocumentation();
     void explorerTabChanged(int idx);
     void setParamDataFrame(QString name);
-    void setParam(QString newParamValue);
+    void setParam(QString paramDataFrame, QString newParamValue);
 
     void setSmallFont();
     void setMediumFont();

easyNet-app/tableeditor.h
@@ -47,7 +47,7 @@ signals:
     void currentKeyChanged(QString key);
     void newTableSelection(QString name);
     void setParamDataFrameSignal(QString);
-    void newParamValueSig(QString);
+    void newParamValueSig(QString,QString);
     void columnDropped(QString set);
     void restoreComboBoxText();
     void newTableName(QString);
@@ -99,6 +99,8 @@ private:
     QWidget         *widget;
     QString         currentTable;
     FindDialog*     findDialog;
+    bool            thisIsParamWindow;
+
 
     void createToolBars();
     void createActions();

commit 86f2dbde52354681d326d8fc0a1133a1f07d279e

bug fix causing crash on release build
- uninitialised variables caused the crash
- bug introduced in commit a739bf99d7058d5fd2920cc4804a7b620b7603e8

easyNet-app/tableeditor.cpp
@@ -63,6 +63,11 @@ TableEditor::TableEditor(ObjectCatalogue *_objectCatalogue, const QString &table
     : QMainWindow (parent)
 {
     // this style of constructor is used for dataframes in the Tables tab
+    tableBox=nullptr;
+    thisIsParamWindow=false;
+    if (tableName=="Parameters")
+        thisIsParamWindow=true;
+
     init(tableName, parent);
     objectCatalogue = _objectCatalogue;
     setFilter("dataframe");