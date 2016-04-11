1431f0195b1bfc45a3da407c561a715be9582519
jsadelman@sky.com <james@Jamess-MacBook-Air.local>
2016-04-11 19:26:58 +0100

    further correct to legal C++

--- easyNet-app/floatingdialogwindow.cpp
+++ easyNet-app/floatingdialogwindow.cpp
@@ -2,7 +2,7 @@
 #include <QDesktopWidget>
 #include <QSize>
 
-FloatingDialogWindow::FloatingDialogWindow(QWidget *parent = 0) : QMainWindow()
+FloatingDialogWindow::FloatingDialogWindow(QWidget *parent) : QMainWindow()
 {
     QRect screenSize = QDesktopWidget().availableGeometry(this);
     this->resize(QSize(screenSize.width() * 0.4f, screenSize.height() * 0.7f));
